dedd0ba428a388fddd112163ad666812994552a9
IzaqueNunes <izaquenunes560@gmail.com>
2022-04-14 10:19:29 -0300

    estructured login

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,8 +1,13 @@
 import React from 'react'
+import { ChakraProvider } from '@chakra-ui/react'
 import { AppProps } from 'next/app'
 
 const MyApp: React.FC<AppProps> = ({ Component, pageProps }) => {
-  return <Component {...pageProps} />
+  return (
+    <ChakraProvider>
+      <Component {...pageProps} />
+    </ChakraProvider>
+  )
 }
 
 export default MyApp
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,17 +1,34 @@
 import React from 'react'
-import Head from 'next/head'
+import {
+  Button,
+  Flex,
+  Heading,
+  Input,
+  useColorMode,
+  useColorModeValue
+} from '@chakra-ui/react'
 
 const Home: React.FC = () => {
-  return (
-    <div>
-      <Head>
-        <title>Homepage</title>
-      </Head>
+  const { toggleColorMode } = useColorMode()
+  const formBackground = useColorModeValue('gray.100', 'gray.700')
 
-      <main>
-        <h1>Hello World</h1>
-      </main>
-    </div>
+  return (
+    <Flex height="100vh" alignItems="center" justifyContent="center">
+      <Flex direction="column" background={formBackground} p={12} rounded={6}>
+        <Heading mb={6}>Login</Heading>
+        <Input
+          placeholder="email@email.com"
+          variant="flushed"
+          mb={3}
+          type="email"
+        />
+        <Input placeholder="senha" variant="flushed" mb={3} type="password" />
+        <Button mb={6} colorScheme="teal">
+          Entrar
+        </Button>
+        <Button onClick={toggleColorMode}>Alterar tema</Button>
+      </Flex>
+    </Flex>
   )
 }
 
