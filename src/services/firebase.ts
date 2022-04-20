c7aff2a7b35747318d71e300f7cbbb3680562039
IzaqueNunes <izaquenunes560@gmail.com>
2022-04-20 15:16:19 -0300

    created dot env

--- src/services/firebase.ts
+++ src/services/firebase.ts
@@ -18,12 +18,12 @@ import {
 } from 'firebase/storage'
 
 const firebaseConfig = {
-  apiKey: 'AIzaSyDeSjZW6Oq1Rh1Me04Jzpc-Sinf88BBipg',
-  authDomain: 'agendaapp-98300.firebaseapp.com',
-  projectId: 'agendaapp-98300',
-  storageBucket: 'agendaapp-98300.appspot.com',
-  messagingSenderId: '1035615029280',
-  appId: '1:1035615029280:web:fb325d0d06e2c2c9c4d445'
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID
 }
 
 const app = initializeApp(firebaseConfig)
